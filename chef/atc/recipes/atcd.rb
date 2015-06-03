a4f5abe32b031ca116fda15557c4a5013ba9d8f2
Jesse Rabek <jesse@airtime.com>
2015-06-02 18:51:01 -0700

    Always NAT
    Makes it simpler to set up when putting the ATC gateway behind an AP

--- chef/atc/recipes/atcd.rb
+++ chef/atc/recipes/atcd.rb
@@ -25,70 +25,64 @@ node['network']['interfaces'].each do |name, eth|
     [name]['arp_announce'] = 2
 end
 
-# If we're in a sandbox, i.e. vagrant, we want to set up NAT
-# But if we're not, we will let the user handle that themselves.
-# We need NAT because we have just a bunch of VMs (atcclients) without a router
-# so the atcd box need to handle the routing/NAT
-if node.vagrant?
+# Always NAT
+# Try to guess the correct interfaces.
+lan_iface = ''
+wan_iface = node['network']['default_interface']
+node['network']['interfaces'].each do |name, eth|
+  next if (eth.encapsulation != 'Ethernet') || (name == wan_iface)
+  lan_iface = name
+  break
+end
 
-  # Try to guess the correct interfaces.
-  lan_iface = ''
-  wan_iface = node['network']['default_interface']
-  node['network']['interfaces'].each do |name, eth|
-    next if (eth.encapsulation != 'Ethernet') || (name == wan_iface)
-    lan_iface = name
-    break
-  end
+if lan_iface == ''
+  log 'You only have 1 network interface. '\
+    'ATCD requires 2 network interfaces. '\
+    'Chef will likely fail.'
+  lan_iface = 'eth1'
+end
 
-  if lan_iface == ''
-    log 'You only have 1 network interface. '\
-      'ATCD requires 2 network interfaces. '\
-      'Chef will likely fail.'
-    lan_iface = 'eth1'
-  end
+log "Using interfaces lan=>#{lan_iface}, wan=>#{wan_iface}"
 
-  log "Using interfaces lan=>#{lan_iface}, wan=>#{wan_iface}"
+# NAT
+include_recipe 'simple_iptables'
+simple_iptables_rule 'nat' do
+  table 'nat'
+  direction 'POSTROUTING'
+  jump 'MASQUERADE'
+  rule "-o #{node['network']['default_interface']}"
+end
 
-  # NAT
-  include_recipe 'simple_iptables'
-  simple_iptables_rule 'nat' do
-    table 'nat'
-    direction 'POSTROUTING'
-    jump 'MASQUERADE'
-    rule "-o #{node['network']['default_interface']}"
-  end
+# DHCP
+# By providing IP on the lan interface,
+# we can get Genymotion instances to route through atc
+require 'ipaddr'
+node.default['dhcp']['interfaces'] = [lan_iface]
+include_recipe 'dhcp::server'
 
-  # DHCP
-  # By providing IP on the lan interface,
-  # we can get Genymotion instances to route through atc
-  require 'ipaddr'
-  node.default['dhcp']['interfaces'] = [lan_iface]
-  include_recipe 'dhcp::server'
+ipv4 = node['network']['interfaces'][lan_iface]['addresses'].select do |_, v|
+  v.family == 'inet'
+end
+ip = ipv4.keys[0]
+ipaddr = IPAddr.new "#{ip}/#{ipv4[ip]['prefixlen']}"
+range_start = ipaddr | 100
+range_end = ipaddr | 200
+dhcp_subnet ipaddr.to_s do
+  range "#{range_start} #{range_end}"
+  broadcast ipv4[ip]['broadcast']
+  netmask ipv4[ip]['netmask']
+  routers [ip]
+end
 
-  ipv4 = node['network']['interfaces'][lan_iface]['addresses'].select do |_, v|
-    v.family == 'inet'
-  end
-  ip = ipv4.keys[0]
-  ipaddr = IPAddr.new "#{ip}/#{ipv4[ip]['prefixlen']}"
-  range_start = ipaddr | 100
-  range_end = ipaddr | 200
-  dhcp_subnet ipaddr.to_s do
-    range "#{range_start} #{range_end}"
-    broadcast ipv4[ip]['broadcast']
-    netmask ipv4[ip]['netmask']
-    routers [ip]
-  end
-
-  # When running under vagrant, atcd depends on the mounts and will not start
-  # unless those are up. The mount is happening after the system is up.
-  # We can use udev to trigger starting/stopping atcd when amount/umount event
-  # happens.
-  template '/etc/udev/rules.d/50-vagrant-mount-atcd.rules' do
-    source 'mount-udev.rules.erb'
-    variables(
-      :service => 'atcd'
-    )
-  end
+# When running under vagrant, atcd depends on the mounts and will not start
+# unless those are up. The mount is happening after the system is up.
+# We can use udev to trigger starting/stopping atcd when amount/umount event
+# happens.
+template '/etc/udev/rules.d/50-vagrant-mount-atcd.rules' do
+  source 'mount-udev.rules.erb'
+  variables(
+    :service => 'atcd'
+  )
 end
 
 include_recipe 'sysctl::apply'
